ad61c09c6b128b7c32d8c7542afdd6c6efe7a3bf
Fernandosoriano <fersoriano@ciencias.unam.mx>
2022-07-11 16:14:05 -0500

    bitwise y closures ejercicios agregados

--- closures/1closuresExcercises.js
+++ closures/1closuresExcercises.js
@@ -40,3 +40,20 @@
     const jobs = ["Butcher", "Programmer", "Doctor", "Teacher", "Lecturer"];
     // assignPersonToJob(names, jobs);
 }
+
+
+{ // 3.-Write a function redundant that takes in a string str and returns a function that 
+    // returns str.
+    const redundant = (str) => {
+        const imprimeString = () => {
+            return str;
+        };
+        return imprimeString;
+    };
+    // const f1 = redundant("apple");
+    // console.log(f1());
+    // const f2 = redundant("pear");
+    // console.log(f2());
+    // const f3 = redundant("");
+    // console.log(f3());
+}
